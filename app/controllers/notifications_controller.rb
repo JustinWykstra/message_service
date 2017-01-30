5f3f385fc13e4bec785dd26e852b1818e365461e
JustinWykstra <justinwyks@gmail.com>
2017-01-30 11:27:45 -0700

    Integrating functionability to create records

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,14 @@
+# app/controllers/notifications_controller.rb
+
 class NotificationsController < ApplicationController
+  def create
+    @notification = Notification.create(params.permit(:phone, :body, :source_app))
+
+    respond_to do |format|
+      format.json { render action: 'show', status: :created, location: @notification}   
+    end
+  end
+
+  def show
+  end
 end
